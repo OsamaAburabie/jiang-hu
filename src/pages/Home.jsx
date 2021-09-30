af136319b09cf66b93a9943301dad4b098c591ac
Osama Aburabie <aburabie.osama@gmail.com>
2021-10-01 00:59:00 +0300

    added protecttion pill

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import Study from '@/components/Study';
 import React, { useEffect, useState } from 'react';
 import './Home.css';
 export default function Home() {
+  const [checked, setChecked] = useState(false);
+  const [prevStar, setPrevStar] = useState(null);
+  const [nextStar, setNextStar] = useState(null);
   const [stars, setStars] = useState([
     {
       num: 1,
@@ -80,17 +83,66 @@ export default function Home() {
     return parseFloat(total(sumOfValues, connected)).toFixed(1);
   };
 
-  //function to add attributes to stars
-  const addAttribute = (num, attributes) => {
-    const newStars = [...stars];
-    newStars[num - 1].attributes = attributes;
-    setStars(newStars);
+  //function to study stars
+  const normalStudy = (num, attributes) => {
+    if (checked === false) {
+      const newStars = [...stars];
+      newStars[num - 1].attributes = attributes;
+      setStars(newStars);
 
-    //set selected star to be the next star if the next star has empty attributes
+      //set selected star to be the next star if the next star has empty attributes
+      if (newStars[num]?.attributes === '') {
+        setSelectedStar(newStars[num]);
+      }
 
-    if (newStars[num]?.attributes === '') {
-      setSelectedStar(newStars[num]);
+      for (let i = 0; i < stars.length - 1; i++) {
+        if (
+          stars[i]?.attributes?.attribute ===
+            stars[i + 1]?.attributes?.attribute &&
+          stars[i]?.attributes !== ''
+        ) {
+          const newStars = [...stars];
+          newStars[i].connected = true;
+          setStars(newStars);
+        } else {
+          const newStars = [...stars];
+          newStars[i].connected = false;
+          setStars(newStars);
+        }
+      }
+    } else if (checked === true) {
+      const oldStar = stars.find((star) => star.num === num);
+      const newStar = attributes;
+      if (oldStar?.attributes) {
+        setPrevStar(oldStar?.attributes);
+        setNextStar(newStar);
+      } else {
+        setPrevStar(null);
+      }
     }
+  };
+
+  //function to set selected star
+  const selectStar = (num) => {
+    //dont allow selecting next star if current and previous star are empty
+    if (stars[num - 1].attributes === '') {
+      return;
+    }
+    const star = stars.filter((star) => star.num === num);
+    setSelectedStar(...star);
+  };
+
+  const keepOld = () => {
+    setPrevStar(null);
+    setNextStar(null);
+  };
+
+  const replace = () => {
+    const newStars = [...stars];
+    newStars[selectedStar.num - 1].attributes = nextStar;
+    setStars(newStars);
+    setPrevStar(null);
+    setNextStar(null);
 
     for (let i = 0; i < stars.length - 1; i++) {
       if (
@@ -108,20 +160,6 @@ export default function Home() {
       }
     }
   };
-
-  //function to set selected star
-  const selectStar = (num) => {
-    //dont allow selecting next star if current and previous star are empty
-    // if (selectedStar.attributes === '') {
-    //   return;
-    // }
-    if (stars[num - 1].attributes === '') {
-      return;
-    }
-    const star = stars.filter((star) => star.num === num);
-    setSelectedStar(...star);
-  };
-
   //function to switch on star colors
   const switchColor = (attribute) => {
     switch (attribute) {
@@ -140,6 +178,7 @@ export default function Home() {
         return 'bg-[#333]';
     }
   };
+
   //function to switch on star border color
   const switchBorderColor = (quality) => {
     switch (quality) {
@@ -160,11 +199,30 @@ export default function Home() {
         return 'border-[12px] border-[#333]';
     }
   };
+  const switchTextColor = (quality) => {
+    switch (quality) {
+      case 'epic':
+        return 'text-[#A83547]';
+      case 'ultra':
+        return 'text-[#E8A651]';
+      case 'elite':
+        return 'text-[#643CA8]';
+      case 'unique':
+        return 'text-[#3A7858]';
+      case 'rare':
+        return 'text-[#6380AA]';
+      case 'normal':
+        return 'text-[##B6B4B2]';
+
+      default:
+        return 'text-[#333]';
+    }
+  };
   return (
     <div className='w-screen h-screen'>
       <div className='w-full h-full bg-gray-600 flex justify-center flex-wrap '>
         <div className='w-full h-[7rem] bg-[#222222] flex items-center justify-center'>
-          {stars.map((star) => (
+          {stars.map((star, index) => (
             <div className='flex items-center' key={star.num}>
               <div
                 onClick={() => selectStar(star.num)}
@@ -182,20 +240,58 @@ export default function Home() {
 
           {/* button to addatts */}
         </div>
-        <div className='w-full h-[calc(100%-7rem)] bg-gray-600  p-2'>
+        <div className='w-full h-[calc(100%-7rem)] bg-gray-800  p-2'>
           <div className='w-full text-lg text-white'>
             <p>M-Attack: {TotalAttribute('M-attack')}</p>
             <p>P-Attack: {TotalAttribute('P-attack')}</p>
             <p>breakthrough: {TotalAttribute('breakthrough')}</p>
             <p>P-strike: {TotalAttribute('P-strike')}</p>
-            <p>HB: {TotalAttribute('HB')}</p>
+            <p>HP: {TotalAttribute('HB')}</p>
           </div>
-          <button
-            className='w-20 h-10 bg-yellow-400 mt-5'
-            onClick={() => addAttribute(selectedStar.num, Study())}
-          >
-            Study
-          </button>
+          <div className='text-white'>
+            <button
+              className='w-20 h-10 bg-yellow-400 mt-5'
+              onClick={() => normalStudy(selectedStar.num, Study())}
+            >
+              Study
+            </button>
+            <form>
+              <label className=' mr-1'>Use Protection pill</label>
+              <input
+                type='checkbox'
+                checked={checked}
+                onChange={(e) => setChecked(e.target.checked)}
+              />
+            </form>
+            {prevStar && (
+              <p className={`${switchTextColor(prevStar.quality)}`}>
+                (previous) {prevStar.attribute} +{prevStar.value} (
+                {prevStar.quality})
+              </p>
+            )}
+            {nextStar && (
+              <p className={`${switchTextColor(nextStar.quality)}`}>
+                (new) {nextStar.attribute} +{nextStar.value} ({nextStar.quality}
+                )
+              </p>
+            )}
+            {prevStar && nextStar && (
+              <>
+                <button
+                  className='h-10 bg-yellow-400 mt-5 mr-2 p-2'
+                  onClick={() => keepOld()}
+                >
+                  Keep old attributes
+                </button>
+                <button
+                  className=' h-10 bg-yellow-400 mt-5 p-2'
+                  onClick={() => replace()}
+                >
+                  Replace
+                </button>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
